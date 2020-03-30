47162a3aa0c7677ead495e71e57b1478d5a34070
Daniel Fong <daniel.fong.mv@gmail.com>
2020-03-30 15:51:23 -0600

    fibonacci

--- index.js
+++ index.js
@@ -68,6 +68,33 @@ function splitter(str, separator) {
 
 console.log(splitter('02/20/2020', '/'))
 
+// Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+
+function fib(num, sequence=[]) {
+    if (num <= sequence.length) {
+        return sequence
+    }
+
+    if (sequence.length === 0 || sequence.length === 1) {
+        sequence.push(1)
+    }
+    else {
+        sequence.push(sequence[sequence.length-2] + sequence[sequence.length-1])
+    }
+
+    return fib(num, sequence);
+}
+
+console.log(fib(7))
+
+function factorial(num) {
+    if (num === 1) {
+        return 1
+    }
+
+
+}
+
 // joses code below
 
 
