3769371feef67ce9c41c4041ce8fc73347502260
Jose <jozylopez27@gmail.com>
2020-03-30 17:11:31 -0500

    factorial

--- index.js
+++ index.js
@@ -154,3 +154,13 @@ function binary(num) {
 }
 
 // console.log(binary(8))
+
+function factorial(num) {
+    if(num <= 1) {
+        return 1
+    }
+
+    return num * factorial(num - 1)
+}
+
+console.log(factorial(5))
